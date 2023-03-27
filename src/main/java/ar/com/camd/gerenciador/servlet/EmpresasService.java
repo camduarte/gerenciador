82e3e6408b8b31bf7aa8f0b1b1d98a1067cb5ab6
cduarte <duarte.camd@gmail.com>
2023-03-27 04:36:19 -0300

    El web service responde con json o xml dependiendo de la solicitud del cliente.

--- src/main/java/ar/com/camd/gerenciador/servlet/EmpresasService.java
+++ src/main/java/ar/com/camd/gerenciador/servlet/EmpresasService.java
@@ -1,9 +1,9 @@
 package ar.com.camd.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
 import ar.com.camd.gerenciador.modelo.DB;
@@ -21,20 +21,26 @@ public class EmpresasService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Empresa> empresas = DB.getListaEmpresas();
 
-		XStream xstream = new XStream();
-		xstream.alias("empresa", Empresa.class); // Reemplazo el nombre completo de la clase con un alias.
-		String xml = xstream.toXML(empresas);
+		String formato = request.getHeader("Accept");
 		
-		PrintWriter out = response.getWriter();
-		response.setContentType("Application/xml");
-		out.print(xml);
-		
-//		Gson gson = new Gson();
-//		String json = gson.toJson(empresas);
-//		
-//		PrintWriter out = response.getWriter();
-//		response.setContentType("Application/json");
-//		out.print(json);
+		if (formato.contains("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("Application/json");
+			response.getWriter().print(json);
+		} else if (formato.contains("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class); // Reemplazo el nombre completo de la clase con un alias.
+			String xml = xstream.toXML(empresas);
+			
+			response.setContentType("Application/xml");
+			response.getWriter().print(xml);
+		} else {
+			response.setContentType("Application/json");
+			response.getWriter().print("{'message': 'no content'}");
+		}
+
 	}
 
 }
